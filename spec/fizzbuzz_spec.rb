f721097866f8e250f4169ed2ba5dfd113777c0de
Kate Young <89363807+katemyoung@users.noreply.github.com>
2021-10-14 13:22:29 +0100

    Write test case 'fizzbuzz'

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,11 +1,14 @@
 require "fizzbuzz"
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passes 3' do
+  it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
-  describe 'fizzbuzz' do
-    it 'returns "fizz" when passes 5' do
-      expect(fizzbuzz(5)).to eq 'buzz'
-    end
+    
+  it 'returns "fizz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
 end
