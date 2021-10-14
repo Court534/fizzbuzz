952e951fc3744c0861dc00efd4ba430f7b2f5f62
Courtney Stow <courtney-stow@hotmail.co.uk>
2021-10-14 13:15:10 +0100

    Rspec still failing Buzz

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,4 +3,9 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passes 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  describe 'fizzbuzz' do
+    it 'returns "fizz" when passes 5' do
+      expect(fizzbuzz(5)).to eq 'buzz'
+    end
+  end
 end
